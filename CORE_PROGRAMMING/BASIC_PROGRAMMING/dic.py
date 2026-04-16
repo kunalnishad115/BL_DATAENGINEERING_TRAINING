1829bd56c6be8ce996e038acc2893a1f6181ea0e
kunalnishad115 <nishadkunal1234567@gmail.com>
2026-04-16 21:38:32 +0530

    todays live review Questions

---
+++ CORE_PROGRAMMING/BASIC_PROGRAMMING/dic.py
@@ -0,0 +1,64 @@
+# n=int(input("Enter a number: "))
+# list=[]
+
+# for i in range(n):
+#    val=input("Enter a value: ")
+#    list.append(val)
+
+# print("List: ",list)
+
+
+# freq={}
+# for item in list:
+#    if item in freq:
+#       freq[item]+=1
+#    else:
+#       freq[item]=1
+
+# print("Frequency of items: ",freq)
+
+# dummy_touple=tuple(freq)
+# print("Dummy touple: ",dummy_touple)
+   
+
+   
+
+# val="enter the val ",a={input("Enter a value: ")},"second val ",b={input("Enter a value: ")}
+# print(a,b)
+
+
+
+
+
+# marks = {
+#     "math": 85,
+#     "science": 99,
+#     "english": 99,
+#     "coding": 99
+# }
+
+# # fun=sum()
+
+
+# freq={}
+
+# for key, val in marks.items():
+#    freq[val]=freq.get(val,0)+1
+#    if freq[val]>=2:
+#       print(f"Marks {val} has frequency {freq[val]}")
+
+# print("Frequency of marks: ",freq)
+
+
+
+# sum=0
+# for key, val in marks.items():
+#    print(f"{key}: {val}")
+#    sum+=val
+
+# print("Total marks: ",sum)
+
+
+
+# marks.setdefault("social", 90)
+# print("Marks after adding social: ",marks)
---
+++ CORE_PROGRAMMING/BASIC_PROGRAMMING/practice.py
@@ -0,0 +1,65 @@
+# Task: Student Eligibility Filter using Sets
+# Problem Statement
+
+# You are given two sets:
+# Students who submitted assignments
+# Students who attended classes
+
+# You need to determine:
+
+# Who is eligible (both submitted + attended)
+# Who is not eligible
+# Who only did one of the activities
+# Input
+# submitted = {"Geeta", "Rahul", "Amit", "Sneha"}
+# attended = {"Amit", "Sneha", "Kiran", "Geeta"}
+# Requirements
+# Find Eligible Students
+
+# Students who are in both
+# Find Not Eligible Students
+
+# Students who are:
+# Only submitted OR
+# Only attended
+
+
+# create data set including studentname,student id,subject,marks and fileter student marks based on taken marks if >50%,70%firstclass >85% distinction ,and also give them grade based percentage "A","B","C
+
+
+dummy_data=[
+  {"name": "kunal", "id": 1, "subject": "Math", "marks": 50},
+  {"name": "rahul", "id": 2, "subject": "Science", "marks": 70},
+  {"name": "Aman", "id": 3, "subject": "English", "marks": 90}
+
+]
+
+
+
+ans_data={
+
+}
+
+for student in dummy_data:
+  if student["marks"]>85:
+    ans_data[student["name"]]="A"
+  elif student["marks"]>70:
+    ans_data[student["name"]]="B"
+  elif student["marks"]>50:
+    ans_data[student["name"]]="C"
+  else:
+    ans_data[student["name"]]="Fail"
+
+print(ans_data)
+
+
+user_input=input("Enter a String: ")
+
+ans_str=""
+
+for char in user_input[::-1]:
+  ans_str+=char
+
+print("Reversed String: ",ans_str)
+
+
