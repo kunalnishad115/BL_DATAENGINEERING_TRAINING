72a9e00724b5e682835b5cfb997eef4354081478
kunalnishad115 <nishadkunal1234567@gmail.com>
2026-05-02 00:07:48 +0530

    basic numpy decleration methods

---
+++ NUMPY_STARTED/NUMPY_BASICS/app.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random
+
+view=np.array([1,23,45,6,7,7543])
+# print(view)
+
+view1=np.arange(1,100)
+# print(view1)
+
+view2=np.arange(1,26).reshape(5,5)
+# print(view2)
+
+view3= np.ones((100,100))
+view4=np.zeros((100,100))
+# print(view3)
+# print(view4,view3)
+
+
+# view5=np.random.random(2,2)
+# print(view5)
+
+view6=np.linspace(-19,19,6)
+# print(view6)
+
+view7=np.identity(4)
+print(view7)
