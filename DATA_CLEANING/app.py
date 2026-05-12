eb699630f4c1df4f537b85456f24ad9436472966
kunalnishad115 <nishadkunal1234567@gmail.com>
2026-05-12 11:05:21 +0530

    data processing how to fill , remove and find the null values in the dataset...

---
+++ DATA_CLEANING/app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+data_set={
+  'id':[1,2,3,4,5,6],
+  'names':['kunal','ram','shyam',np.nan,np.nan,'rahul'],
+  'marks':[90,97,np.nan,np.nan,np.nan,89]
+}
+
+student=pd.DataFrame(data_set)
+print(student)
+
+
+## find the missing values
+
+print(pd.isna(student).sum())
+
+## remove the missing values
+
+print(student.dropna())
+
+## filling missing values
+student.loc[student['names'].isnull(), 'names'] = ['karan', 'kirti']
+
+print(student)
+
