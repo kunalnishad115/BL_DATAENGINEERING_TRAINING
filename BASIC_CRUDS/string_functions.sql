acc9ec58f71b3a23960355df18f298b875a9c7ee
kunalnishad115 <nishadkunal1234567@gmail.com>
2026-05-29 12:26:04 +0530

    add the diagram of Task. and Flow of Task ..

---
+++ BASIC_CRUDS/string_functions.sql
@@ -0,0 +1,13 @@
+-- select * from bank;
+
+-- select emp_id , concat_ws('-',fname,lname) as FullName from bank;
+
+--select substr('hello buddy',1,5);
+
+-- select reverse(fname) as ulte from bank;
+
+-- select fname as out from bank where length(fname)>5;
+
+-- select length(' kunal   '),length(TRIM(' kunal   '));
+
+
