8e3358426bc8bbf5176ee0ae8ea517108eae6b99
kunalnishad115 <nishadkunal1234567@gmail.com>
2026-05-29 14:27:50 +0530

    complete the task 5 and Task 6 completed...

---
+++ MIS_TASK_APP/task_5_task_6.sql
@@ -0,0 +1,24 @@
+ALTER TABLE patients
+ADD COLUMN blood_group VARCHAR(5);
+
+ALTER TABLE patients
+RENAME COLUMN phone TO mobile_number;
+
+DROP TABLE engagement_summary;
+
+TRUNCATE TABLE user_engagement;
+
+UPDATE patients
+SET city = 'Bangalore'
+WHERE patient_id = 1;
+
+DELETE FROM appointments
+WHERE status = 'Cancelled';
+
+INSERT INTO user_engagement
+(patient_id, login_count, session_duration, last_login, device_type)
+VALUES
+(3, 5, 35, CURRENT_TIMESTAMP, 'Mobile'),
+(7, 2, 20, CURRENT_TIMESTAMP, 'Laptop'),
+(10, 8, 50, CURRENT_TIMESTAMP, 'Tablet');
+
